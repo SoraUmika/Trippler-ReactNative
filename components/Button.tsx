61285a8684295cf9d5c7382885dddb13daa0556a
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-12 23:41:18 -0500

    Added Button widget.

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,83 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
 
-interface Props{
-    type: "normal",
-    text: string
+interface Props {
+	text: string;
+	bgColor: string;
+	shadowColor: string;
+	width?: number;
+	height?: number;
+	onPress?: Function;
 }
 
-export default class Button extends Component<Props>{
-    render(){
-        return (
-            <button/>
-        )
-    }
+interface State {
+	down: boolean;
 }
+
+export default class Button_ extends Component<Props, State> {
+	state: State = {
+		down: false
+	};
+
+	onTouchDown = () => {
+        this.setState({ down: true });
+        this.props.onPress && this.props.onPress()
+	};
+
+	onTouchUp = () => this.setState({ down: false });
+
+	render() {
+		const { text, bgColor, width, height, shadowColor } = this.props;
+		const { down } = this.state;
+		return (
+			<View>
+				<View
+					style={{
+						...styles.button,
+						backgroundColor: bgColor,
+						width: width,
+						height: height
+					}}
+					onTouchStart={this.onTouchDown}
+					onTouchEnd={this.onTouchUp}
+				>
+					<View style={styles.textContainer}>
+						<Text style={styles.text}>{text}</Text>
+					</View>
+				</View>
+				{!down && (
+					<View
+						style={{
+							...styles.shadow,
+							backgroundColor: shadowColor
+						}}
+					/>
+				)}
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	button: {
+		borderRadius: 8,
+		zIndex: 2
+	},
+	textContainer: {
+		flex: 1,
+		justifyContent: "center",
+		shadowColor: "#000",
+		shadowOffset: { width: 0, height: 100 },
+		shadowOpacity: 1
+	},
+	text: {
+		textAlign: "center"
+	},
+	shadow: {
+		height: 16,
+		borderBottomLeftRadius: 8,
+		borderBottomRightRadius: 8,
+		top: -8,
+		zIndex: 1
+	}
+});
