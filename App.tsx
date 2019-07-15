e34e941d578531c724b69aaa3d3fe076fafc1709
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-14 22:46:51 -0500

    just added one line.... to change the icon colors in the status bar in android.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { createStackNavigator, createAppContainer } from "react-navigation";
+import { StatusBar } from "react-native";
 import store from "./src/redux/store";
 
 import LoginScreen from "./src/components/LoginScreen";
 import SignupScreen from "./src/components/SignupScreen";
+
+StatusBar.setBarStyle("dark-content");
 
 const Navigator = createAppContainer(
 	createStackNavigator(
