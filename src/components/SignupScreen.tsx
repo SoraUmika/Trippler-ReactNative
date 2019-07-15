1e7dc11a0a981fc3a2ab116bce5160b187f876ca
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-14 19:02:23 -0500

    Added SignupScreen component.

---
+++ src/components/SignupScreen.tsx
@@ -0,0 +1,82 @@
+import React, { FC, useState, useEffect } from "react";
+import { StyleSheet, View, Text, KeyboardAvoidingView, ScrollView, StatusBar } from "react-native";
+
+import Button from "./Button";
+import Input from "./Input";
+
+const SignupScreen: FC = props => {
+	const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [rePassword, setRePassword] = useState("");
+    const [email, setEmail] = useState("");
+
+	useEffect(() => {
+		StatusBar.setBackgroundColor("#ffffff");
+		StatusBar.setBarStyle("dark-content");
+	});
+
+	return (
+		<ScrollView style={styles.container}>
+			<View style={styles.titleContainer}>
+				<Text style={styles.title}>Sign Up</Text>
+			</View>
+			<KeyboardAvoidingView style={styles.formContainer} enabled behavior="padding">
+				<Input
+					width="80%"
+					placeholder="User name"
+					onChange={e => setUserName(e.nativeEvent.text)}
+					textContentType="username"
+				/>
+                <Input
+					width="80%"
+					placeholder="Email"
+					onChange={e => setEmail(e.nativeEvent.text)}
+					textContentType="emailAddress"
+				/>
+				<Input
+					width="80%"
+					placeholder="Password"
+					onChange={e => setPassword(e.nativeEvent.text)}
+					textContentType="newPassword"
+					secureTextEntry
+				/>
+                <Input
+					width="80%"
+					placeholder="Re-enter password"
+					onChange={e => setRePassword(e.nativeEvent.text)}
+					textContentType="password"
+					secureTextEntry
+				/>
+                <Button
+					width="80%"
+					height={50}
+					text="Confirm"
+					color="black"
+                    textStyle={{ color: "white" }}
+					onPress={() => console.log(`${userName}, ${email}, ${password}`)}
+				/>
+			</KeyboardAvoidingView>
+		</ScrollView>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {},
+	titleContainer: {
+		flex: 1,
+		height: 100,
+		justifyContent: "center"
+	},
+	title: {
+		fontSize: 32,
+		textAlign: "center"
+	},
+	formContainer: {
+		flex: 1,
+        justifyContent: "space-around",
+        alignItems: "center",
+		height: 350
+    }
+});
+
+export default SignupScreen;
