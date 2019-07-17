b0648da5fa4c26a3f1c80bad2ded3afba1a863eb
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-17 15:34:10 -0500

    Added svgConverter.py, and added svg icon MoreHoriz.

---
+++ src/svg/MoreHoriz.tsx
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import Svg, {Path, SvgProps} from "react-native-svg";
+
+export default class MoreHoriz extends Component<SvgProps> {
+    
+    shouldComponentUpdate() {
+        return false;
+    }
+    
+    render() {
+        return (
+            <Svg width={24} height={24} {...this.props}>
+                <Path fill="none" d="M0 0h24v24H0V0z" />
+                <Path d="M6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
+            </Svg >
+        )
+    }
+
+}
+
---
+++ src/svgConverter.py
@@ -0,0 +1,109 @@
+import sys
+from os.path import dirname, join
+
+
+def to_abs_path(*paths):
+    return join(dirname(__file__), *paths)
+
+
+def get_component_name(svg_name):
+    name = svg_name.split("-")[1]  # twotone-more_horiz-24px.svg -> more_horiz
+    words = name.split("_")  # more_horiz -> [more, horiz]
+    capped = [word.capitalize() for word in words]  # [more, horiz] -> [More, Horiz]
+    return "".join(capped)  # [More, Horiz] -> MoreHoriz
+
+
+def split_tags(svg_text_):
+    return [
+        split_tag(tag_)for tag_ in  # <a a=a -> <a, a=a
+        svg_text_.split(">")[:-1]  # <.><./><./></.> -> <, <./, <./, </.
+    ]
+
+
+def svg_tag_to_jsx(svg_tags):
+    new_jsx_tags = []
+    for i, tag_ in enumerate(svg_tags):
+        name, *props = tag_
+        if name.startswith("</"):
+            name = "</" + name[2:].capitalize()  # </tag -> </Tag
+        else:
+            name = "<" + name[1:].capitalize()  # <tag -> <Tag
+        new_props = []
+        if not i:  # if it is the start svg tag.
+            for prop in props:
+                prop_name, val = prop.split("=")
+                if prop_name not in ["xmlns", "viewBox"]:  # skip those props.
+                    val = "{" + val + "}"  # 12 -> {12}
+                    new_props.append("=".join((prop_name, val)))
+            new_props.append("{...this.props}>")
+        else:
+            for prop in props:
+                prop_name, val = prop.split("=")
+                val = '"' + val + '"'  # 12 -> "12"
+                new_props.append("=".join((prop_name, val)))
+            if i < len(svg_tags) - 1:
+                new_props.append("/>")
+            else:
+                new_props.append(">")
+        props = new_props
+        new_jsx_tags.append(" ".join([name] + props))
+    return new_jsx_tags
+
+
+def split_tag(svg_tag):
+    splited = svg_tag.split(" ")
+    name = splited[0]
+    prop_str = " ".join(splited[1:])
+    segs = prop_str.split('"')
+    props = []
+    for i, seg in enumerate(segs):
+        if not i % 2 and seg.strip() and seg != "/":
+            props.append(seg.strip() + segs[i + 1])
+    return [name] + props
+
+
+def format_jsx(jsx_tags_):
+    formatted = ""
+    for i, tag in enumerate(jsx_tags_):
+        if not i:
+            formatted += "    " * 3 + tag + "\n"
+        elif i == len(jsx_tags_) - 1:
+            formatted += "    " * 3 + tag
+        else:
+            formatted += "    " * 4 + tag + "\n"
+    return formatted
+
+
+# The name of the svg file to convert.
+SVG_FILE_NAME = sys.argv[1]
+if not SVG_FILE_NAME.endswith(".svg"):
+    SVG_FILE_NAME += ".svg"
+SVG_FILE_PATH = to_abs_path("svg", SVG_FILE_NAME)
+
+COMPONENT_NAME = get_component_name(SVG_FILE_NAME)
+COMPONENT_FILE_PATH = to_abs_path("svg", COMPONENT_NAME + ".tsx")
+
+template = """import React, {Component} from "react";
+import Svg, {Path, SvgProps} from "react-native-svg";
+
+export default class %s extends Component<SvgProps> {
+    
+    shouldComponentUpdate() {
+        return false;
+    }
+    
+    render() {
+        return (
+%s
+        )
+    }
+
+}
+
+"""
+
+with open(SVG_FILE_PATH, "r") as svg_file:
+    svg_text = svg_file.read()
+    jsx_tags = svg_tag_to_jsx(split_tags(svg_text))
+    with open(COMPONENT_FILE_PATH, "w") as comp_file:
+        comp_file.write(template % (COMPONENT_NAME, format_jsx(jsx_tags)))
