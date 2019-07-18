fc568c658118f2d73307ee46588b1106a43a412d
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-18 18:28:10 -0500

    Added dummy data to the initial state of businesses state.

--- src/redux/initState.ts
+++ src/redux/initState.ts
@@ -1,11 +1,14 @@
 import State from "./state";
+import Business from "../Business";
 
 const init_state: State = {
 	theme: {
 		accentColor: "#058ED9"
 	},
 	businesses: {
-		
+		"test": new Business("test", "Egg", "open", 4.3, 123),
+		"hello": new Business("hello", "Foo", "close", 3.1, 10),
+		":)": new Business(":)", "Bar", "open", 4.7, 2031)
 	}
 };
 
