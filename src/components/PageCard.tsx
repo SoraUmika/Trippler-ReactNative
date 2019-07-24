8e745e87e425a002cd56242d99b79c76f93c9a5f
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-23 19:08:09 -0500

    refactored PageCard to functional component.

--- src/components/PageCard.tsx
+++ src/components/PageCard.tsx
@@ -8,7 +8,7 @@
  * @param {TriggerMargin} triggerMargin The margin in which expand/collapse is triggered. More below.
  * @param {StyleProp<ViewStyle>} [rootStyle] The style applied to the root View.
  * @param {StyleProp<ViewStyle>} [containerStyle] The style applied to the children container View.
- * 
+ *
  * TriggerMargin object
  * ====================
  * @param {number} unExpand The expanded -> normal trigger margin.
@@ -16,7 +16,7 @@
  * @param {number} collapse The normal -> collapsed trigger margin.
  * @param {number} unCollapse The collapsed -> normal trigger margin.
  */
-import React, { Component } from "react";
+import React, { FC } from "react";
 import { StyleSheet, Animated, StyleProp, ViewStyle, View } from "react-native";
 import {
 	PanGestureHandler,
@@ -43,108 +43,99 @@ interface TriggerMargin {
 	unExpand: number;
 }
 
-export default class PageCard extends Component<Props> {
-	translateYPan = new Animated.Value(0);
-	// translateYCard = new Animated.Value(0);
-	yCoord = dimension.height(this.props.topPercent);
-	originY = this.yCoord;
-	translateYRange = [
-		-this.yCoord + this.props.topMargin,
+const PageCard: FC<Props> = props => {
+	const translateYPan = new Animated.Value(0);
+	const yCoord = dimension.height(props.topPercent);
+	const translateYRange = [
+		-yCoord + props.topMargin,
 		0,
-		dimension.height(1 - this.props.topPercent) - this.props.bottomPadding
+		dimension.height(1 - props.topPercent) - props.bottomPadding
 	];
-	currentIndex = 1;
-	toValue = 0;
-	triggered = false;
+	let currentIndex = 1;
+	let toValue = 0;
+	let triggered = false;
 
-	constructor(props: Props) {
-		super(props);
-		const { triggerMargin } = props;
-		this.translateYPan.addListener(({ value }) => {
-			const val = value - this.translateYRange[this.currentIndex];
-			if (!this.triggered) {
-				let direction = 0;
-				switch (this.currentIndex) {
-					case 0:
-						if (val >= triggerMargin.unExpand) {
-							direction = 1;
-						}
-						break;
-					case 1:
-						if (val <= -triggerMargin.expand) {
-							direction = -1;
-						} else if (val >= triggerMargin.collapse) {
-							direction = 1;
-						}
-						break;
-					case 2:
-						if (val <= -triggerMargin.unCollapse) {
-							direction = -1;
-						}
-						break;
-				}
-				this.toValue =
-					this.translateYRange[this.currentIndex + direction] -
-					this.translateYRange[this.currentIndex];
-				this.currentIndex += direction;
-				this.triggered = direction != 0;
+	const { triggerMargin } = props;
+
+	translateYPan.addListener(({ value }) => {
+		const val = value - translateYRange[currentIndex];
+		console.log(val);
+		if (!triggered) {
+			let direction = 0;
+			switch (currentIndex) {
+				case 0:
+					if (val >= triggerMargin.unExpand) {
+						direction = 1;
+					}
+					break;
+				case 1:
+					if (val <= -triggerMargin.expand) {
+						direction = -1;
+					} else if (val >= triggerMargin.collapse) {
+						direction = 1;
+					}
+					break;
+				case 2:
+					if (val <= -triggerMargin.unCollapse) {
+						direction = -1;
+					}
+					break;
 			}
-		});
-	}
 
-	onPanEvent = Animated.event([
+			toValue = translateYRange[currentIndex + direction] - translateYRange[currentIndex];
+			currentIndex += direction;
+			triggered = direction != 0;
+		}
+	});
+
+	const onPanEvent = Animated.event([
 		{
 			nativeEvent: {
-				translationY: this.translateYPan
+				translationY: translateYPan
 			}
 		}
 	]);
 
-	onPanStateChange = (event: PanGestureHandlerStateChangeEvent) => {
+	const onPanStateChange = (event: PanGestureHandlerStateChangeEvent) => {
 		if (event.nativeEvent.oldState == State.ACTIVE) {
-			Animated.timing(this.translateYPan, {
-				toValue: this.toValue
+			Animated.timing(translateYPan, {
+				toValue: toValue
 			}).start(() => {
-				this.translateYPan.setOffset(this.translateYRange[this.currentIndex]);
-				this.triggered = false;
+				translateYPan.setOffset(translateYRange[currentIndex]);
+				triggered = false;
 			});
 		}
 	};
 
-	render() {
-		const { children, rootStyle, containerStyle, backgroundColor } = this.props;
-		return (
-			<Animated.View
-				style={{
-					...(rootStyle as object),
-					...styles.root,
-					top: this.yCoord,
-					backgroundColor: backgroundColor,
-					transform: [
-						{
-							translateY: this.translateYPan.interpolate({
-								inputRange: this.translateYRange,
-								outputRange: this.translateYRange,
-								extrapolate: "clamp"
-							})
-						}
-					]
-				}}
-			>
-				<PanGestureHandler
-					onGestureEvent={this.onPanEvent}
-					onHandlerStateChange={this.onPanStateChange}
-				>
-					<View style={styles.handleBarContainer}>
-						<View style={styles.handleBar} />
-					</View>
-				</PanGestureHandler>
+	const { children, rootStyle, containerStyle, backgroundColor } = props;
+	return (
+		<Animated.View
+			style={{
+				...(rootStyle as object),
+				...styles.root,
+				top: yCoord,
+				backgroundColor: backgroundColor,
+				transform: [
+					{
+						translateY: translateYPan.interpolate({
+							inputRange: translateYRange,
+							outputRange: translateYRange,
+							extrapolate: "clamp"
+						})
+					}
+				]
+			}}
+		>
+			<PanGestureHandler onGestureEvent={onPanEvent} onHandlerStateChange={onPanStateChange}>
+				<View style={styles.handleBarContainer}>
+					<View style={styles.handleBar} />
+				</View>
+			</PanGestureHandler>
 
-				<View style={[containerStyle, styles.childrenContainer]}>{children}</View>
-			</Animated.View>
-		);
-	}
-}
+			<View style={[containerStyle, styles.childrenContainer]}>{children}</View>
+		</Animated.View>
+	);
+};
 
 const styles = StyleSheet.create({
 	root: {
@@ -172,3 +163,5 @@ const styles = StyleSheet.create({
 		flex: 1
 	}
 });
+
+export default PageCard;
