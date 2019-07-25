20ca11262c1c13cda24bd4ba637dbb074c6a02a7
John Liu <SoraUmika>
2019-07-25 17:57:21 -0400

    TEST

--- src/majorScreens/BusinessScreen/Info.tsx
+++ src/majorScreens/BusinessScreen/Info.tsx
@@ -1,12 +1,34 @@
 import React, {Component} from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 
 export default class BusinessInfo extends Component{
-    render(){
-      return(
-        <View>
-  
-        </View>    
-      )    
-    }
+  render(){
+    return(
+      <View style={styles.layoutRule}>
+        <Text style={styles.businessName}> Bussiness Name </Text>
+        <Text style={styles.address}> Ratings </Text>
+        <Text style={styles.address}> 3825 Parsons Blvd, NY, 11354 </Text>
+      </View>    
+    )    
   }
+}
+
+const styles = StyleSheet.create({
+  layoutRule: {
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+
+  businessName: {
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: 'white'
+  },
+
+  address: {
+    fontSize: 15,
+    fontStyle: 'normal',
+    color: 'white'
+  }
+
+})
