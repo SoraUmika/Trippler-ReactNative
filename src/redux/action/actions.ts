9c51bd10ec9364b81176df01cca27ad4c00e44af
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-25 11:39:33 -0500

    Added action 'toNextRecom'.

--- src/redux/action/actions.ts
+++ src/redux/action/actions.ts
@@ -1,3 +1,4 @@
 import {createStandardAction} from "typesafe-actions"
 
 export const setAccentColor = createStandardAction("theme/accentColor/SET")<string>();
+export const toNextRecom = createStandardAction("recommendation/currentIndex/INNC")();
