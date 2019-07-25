1cb9f5bdc745eed5cddd86805aa44450eded9a9b
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-25 14:22:29 -0500

    Added icon and style to bottom navigator.

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Provider } from "react-redux";
 import { createAppContainer, createBottomTabNavigator } from "react-navigation";
 import store from "./src/redux/store";
-import {StatusBar} from "react-native";
+import { StatusBar } from "react-native";
 
 import MainScreen from "./src/majorScreens/MainScreen";
 import transitionConfig from "./src/transition";
-import SwipeScreen from "./src/majorScreens/BusinessScreen"
+import BusinessScreen from "./src/majorScreens/BusinessScreen";
+import BookmarkBorder from "./src/svg/BookmarkBorder";
 
 StatusBar.setHidden(false);
 
@@ -14,15 +15,31 @@ const MainNavigator = createAppContainer(
 	createBottomTabNavigator(
 		{
 			// Register screens here.
-			SwipeScreen: { screen: SwipeScreen},	
-			Main: { screen: MainScreen },
+			Business: {
+				screen: BusinessScreen,
+				navigationOptions: {
+					tabBarIcon: ({ tintColor }: any) => <BookmarkBorder fill={tintColor} />
+				}
+			},
+			Main: {
+				screen: MainScreen,
+				navigationOptions: {
+					tabBarIcon: ({ tintColor }: any) => <BookmarkBorder fill={tintColor} />
+				}
+			}
 		},
 		{
-			initialRouteName: 'SwipeScreen'
+			initialRouteName: "Business",
+			backBehavior: "history",
+			tabBarOptions: {
+				activeTintColor: store.getState().theme.accentColor,
+				labelStyle: {
+					fontWeight: "bold"
+				}
+			}
 		}
 	)
 );
-
 
 export default function App() {
 	return (
