dcf23929c48b8fab5d8fb0e5d6dd4f34c383c5f9
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-25 22:54:19 -0500

    Added sort and sortedInsert util functions.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
-import { StatusBar, View } from "react-native";
-import { getStatusBarHeight } from "react-native-status-bar-height";
+import { StatusBar } from "react-native";
 
 import RootNavigator from "./src/navigators/RootNavigator";
 
@@ -11,7 +10,6 @@ StatusBar.setHidden(false);
 export default function App() {
 	return (
 		<Provider store={store}>
-			<View style={{width: "100%", height: getStatusBarHeight(), backgroundColor: "black"}}/>
 			<RootNavigator />
 		</Provider>
 	);
--- src/util.ts
+++ src/util.ts
@@ -1,23 +1,49 @@
 export function objectsEqual(a: any = {}, b: any = {}) {
-    let aProps = Object.getOwnPropertyNames(a);
-    let bProps = Object.getOwnPropertyNames(b);
+	let aProps = Object.getOwnPropertyNames(a);
+	let bProps = Object.getOwnPropertyNames(b);
 
-    if (aProps.length != bProps.length) {
-        return false;
-    }
+	if (aProps.length != bProps.length) {
+		return false;
+	}
 
-    for (let i = 0; i < aProps.length; i++) {
-        let propName = aProps[i];
-        let va = a[propName], vb = b[propName]
-        let type = typeof va;
+	for (let i = 0; i < aProps.length; i++) {
+		let propName = aProps[i];
+		let va = a[propName],
+			vb = b[propName];
+		let type = typeof va;
 
-        if (type == "object") {
-            if (!objectsEqual(va, vb)){
-                return false;
-            }
-        } else if (type != "function" && va != vb){
-            return false;
-        }
-    }
-    return true;
+		if (type == "object") {
+			if (!objectsEqual(va, vb)) {
+				return false;
+			}
+		} else if (type != "function" && va != vb) {
+			return false;
+		}
+	}
+	return true;
+}
+
+type IsOrderedCompare<T> = (left: T, right: T) => any
+
+export function sort<T>(arr: T[], isOrdered: IsOrderedCompare<T>) {
+	for (let pass = 1; pass < arr.length; pass++) {
+		sortPass(arr, isOrdered, pass);
+	}
+}
+
+function sortPass<T>(arr: T[], isOrdered: IsOrderedCompare<T>, pass: number) {
+	let holder = arr[pass];
+	let i = pass;
+	while (i > 0 && !isOrdered(arr[i - 1], holder)) {
+		arr[i] = arr[i - 1];
+		i--;
+	}
+	if (i != pass) {
+		arr[i] = holder;
+	}
+}
+
+export function sortedInsert<T>(arr: T[], val: T, isOrdered: IsOrderedCompare<T>) {
+    const length = arr.push(val);
+    sortPass(arr, isOrdered, length - 1);
 }
