dfbc3b1de0537f1fd2dbb7634367fb76af5cc434
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-25 23:17:23 -0500

    Added actions createStandardAction and createStandardAction.

--- src/redux/action/actions.ts
+++ src/redux/action/actions.ts
@@ -1,5 +1,10 @@
-import {createStandardAction} from "typesafe-actions"
+import { createStandardAction } from "typesafe-actions";
+import { SortMethod } from "../businessSortCompare";
 
 export const setAccentColor = createStandardAction("theme/accentColor/SET")<string>();
 
 export const toNextRecom = createStandardAction("recommendation/currentIndex/INNC")();
+
+export const setCollectSortMethod = createStandardAction("collection/sortMethod/SET")<SortMethod>();
+
+export const saveBusiness = createStandardAction("collection/items/ADD")<string>();
