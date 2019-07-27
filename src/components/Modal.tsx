cba5383a34845211c6fed2acdfaee0966daae7a5
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-27 10:20:34 -0500

    Added Modal component.

---
+++ src/components/Modal.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import {
+	Modal as BuiltInModal,
+	View,
+	StyleSheet,
+	StyleProp,
+	ViewStyle,
+	ModalProps
+} from "react-native";
+
+interface Props extends Omit<ModalProps, "transparent"> {
+	onHide: () => void;
+	style: StyleProp<ViewStyle>;
+}
+
+export default class Modal extends Component<Props> {
+	render() {
+		const { onHide, style, children, ...restProps } = this.props;
+
+		return (
+			<BuiltInModal {...restProps} transparent={true}>
+				<View style={styles.background} onTouchStart={onHide}>
+					<View style={style} onTouchStart={evt => evt.stopPropagation()}>
+						{children}
+					</View>
+				</View>
+			</BuiltInModal>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	background: {
+		width: "100%",
+		height: "100%",
+		backgroundColor: "rgba(0,0,0,0.1)",
+		zIndex: 1
+	}
+});
