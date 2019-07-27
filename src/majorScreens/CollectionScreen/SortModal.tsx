d76e13c6fa9bbf56fc93ab9efa376285b328a3b4
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-27 09:21:11 -0500

    Added SortModal skeleton code.

---
+++ src/majorScreens/CollectionScreen/SortModal.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+import { Modal, View, Text, StyleSheet } from "react-native";
+
+interface Props {}
+
+const SortModal: FC<Props> = props => {
+	return (
+		<Modal>
+			<View />
+		</Modal>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+					
+export default styles;
