efd3d708de428524fba62ea74e36f98f7560bc7b
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-27 17:25:48 -0500

    CollectionList is now implemented with FlatList.

--- src/majorScreens/CollectionScreen/CollectionList.tsx
+++ src/majorScreens/CollectionScreen/CollectionList.tsx
@@ -5,7 +5,7 @@
  * TODO edge transparency.
  */
 import React, { FC } from "react";
-import { StyleSheet, ScrollView } from "react-native";
+import { StyleSheet, ScrollView, FlatList } from "react-native";
 import { useSelector } from "react-redux";
 import {
 	getCollectionItems,
@@ -17,16 +17,29 @@ import CollectionItem from "./CollectionItem";
 
 const CollectionList: FC = () => {
 	const items = useSelector(getCollectionItems);
-	const itemsPinned = useSelector(getCollectionItemsPinned);
 	const showPin = useSelector(getCollectionShowPin);
+	const itemsPinned = useSelector(getCollectionItemsPinned);
+	const pinnedItemLength = itemsPinned.length;
 	return (
-		<ScrollView style={styles.container} bounces indicatorStyle="black">
-			{showPin &&
-				itemsPinned.map(id => <CollectionItem businessId={id} pinned key={id} showPin />)}
-			{items.map(id => (
-				<CollectionItem businessId={id} key={id} showPin={showPin} />
-			))}
-		</ScrollView>
+		// <ScrollView style={styles.container} bounces indicatorStyle="black">
+		// 	{showPin &&
+		// 		itemsPinned.map(id => <CollectionItem businessId={id} pinned key={id} showPin />)}
+		// 	{items.map(id => (
+		// 		<CollectionItem businessId={id} key={id} showPin={showPin} />
+		// 	))}
+		// </ScrollView>
+		<FlatList
+			data={[...(showPin ? itemsPinned : []), ...items]}
+			// data={[...(showPin ? itemsPinned : []), ...items, ...items, ...items, ...items, ...items]}
+			renderItem={({ item, index }) => {
+				if (showPin && index < pinnedItemLength) {
+					return <CollectionItem businessId={item} pinned showPin />;
+				}
+				return <CollectionItem businessId={item} showPin={showPin} />;
+			}}
+			keyExtractor={item => item}
+			// keyExtractor={(item, index) => `${index}`}
+		/>
 	);
 };
 
