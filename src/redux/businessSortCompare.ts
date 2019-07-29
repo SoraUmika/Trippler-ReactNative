af737121148649f28056cf4eaf06d50938a71976
Pucaet <37044557+Pucaet@users.noreply.github.com>
2019-07-28 22:56:14 -0500

    Added some documentations.

--- src/redux/businessSortCompare.ts
+++ src/redux/businessSortCompare.ts
@@ -1,15 +1,36 @@
+/**
+ * Provide the sort compare function used to sort an array of businesses
+ * represented in their ids.
+ * A sort compare or order compare function is a function that takes in a left and
+ * right values, and determines if they are in right order.
+ * 
+ * See the `sort` and `sortedInsert` in util/array. 
+ */
 import Business from "./state/Business";
 import { StrObj } from "../util/type";
 import { IsOrderedCompare } from "../util/array";
 
 export type SortMethod = "name" | "rating" | "avgRating";
 
+/**
+ * Stores the sort compare functions for Business objects.
+ * A map between sort method and sort compare function.
+ */
 export const BusinessCompareFunctions: StrObj<IsOrderedCompare<Business>> = {
 	name: (l, r) => l.name <= r.name,
 	rating: (l, r) => l.rating >= r.rating,
 	avgRating: (l, r) => l.rating / l.ratingNum >= r.rating / r.ratingNum
 };
 
+/**
+ * Returns a sort compare function that is used to sort an array of
+ * Business ids with given sorting method.
+ * 
+ * @param sortMethod The method of sorting. 
+ * @param businesses The map between business id and Business object.
+ * @returns A sort compare function that is used to sort an array of 
+ * 			Business ids with given sorting method.
+ */
 export default function getCompareFunc(
 	sortMethod: SortMethod,
 	businesses: StrObj<Business>
--- src/util/type.ts
+++ src/util/type.ts
@@ -1,13 +1,46 @@
 export type StrObj<T> = { [index: string]: T };
 
+/**
+ * Represent a range between two values.
+ */
 export class Range<T> {
+	/**
+	 * Create a Range object that represent a range between the
+	 * given from value and to value.
+	 * It also takes in a compare function, which is used for the `include` method.
+	 * A compare function is a function that takes in two values, a and b, and returns a number,
+	 * negative if a < b, 0 if a = b, positive if a > b.
+	 * 
+	 * @param from The start value of the range.
+	 * @param to The end value of the range.
+	 * @param compare The compare function, see the constructor description.
+	 */
 	constructor(public from: T, public to: T, public compare: (a: T, b: T) => number) {}
+
+	/**
+	 * Determine if given value is included in the range.
+	 * 
+	 * @param val The value to test if included in the range.
+	 * @returns True if the given value is included in the range, else false.
+	 */
 	include(val: T) {
 		return this.compare(this.from, val) <= 0 && this.compare(val, this.to) <= 0;
 	}
 }
 
+/**
+ * A range between two numbers.
+ * 
+ * @extends Range<number>
+ */
 export class NumberRange extends Range<number> {
+	/**
+	 * Create a NumberRange object that represent a range between given
+	 * from number and to number.
+	 * 
+	 * @param from The start number of the range.
+	 * @param to The end number of the range.
+	 */
 	constructor(from: number, to: number) {
 		super(from, to, (a, b) => a - b);
 	}
