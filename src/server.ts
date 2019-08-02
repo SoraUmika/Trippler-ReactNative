ee25a7e607523c880d12f6dfed17c594f5ce5373
SoraUmika <dazzlejohn1212@gmail.com>
2019-08-02 01:23:00 -0400

    added server basic layout

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,25 @@
-class Server {
-	constructor() {}
+interface action{
+	type: string,
+	info: {}
 }
 
-export default new Server();
+const request = (url='http://ec2-35-168-17-106.compute-1.amazonaws.com:3000', action: action) => {
+	switch(action.type){
+		case 'INSERT_NEW_USER_TO_DATABASE':
+			fetch(url)
+			.then(res => res.text())
+			.then(body => console.log(body))
+			break;
+		case 'CHECK_LOGIN_CREDENTIALS':
+			break;
+		case 'GRAB_RANDOM_RESTAURANT':
+			break;
+		case 'SEARCH_RESTAURANT_BY_KEYWORD':
+			break;
+		case 'UPDATE_USER_ACTIONS':
+			break;
+	}
+	return action
+}
+
+export default request
