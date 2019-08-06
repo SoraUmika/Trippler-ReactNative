48d2022198bd52b6511dff78a29d6e0256e2f7ca
SoraUmika <dazzlejohn1212@gmail.com>
2019-08-05 23:54:24 -0400

    begin usage of middlewares

--- src/redux/reducer/requests.ts
+++ src/redux/reducer/requests.ts
@@ -2,5 +2,8 @@ import RootAction from "../action";
 import { Requests } from "../state";
 
 export default function requests(state: Requests, actions: RootAction): Requests {
-	return state;
+    switch(actions.type){
+        default:
+            return state;
+    }
 }
